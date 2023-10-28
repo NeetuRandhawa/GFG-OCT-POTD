565d6b0459d2b92b57e70c31ab40a3cc3170732a
Nitika choudhary <138247014+NeetuRandhawa@users.noreply.github.com>
2023-10-28 20:45:03 +0530

    Create DAY28.cpp

---
+++ DAY28.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+	int is_bleak(int n)
+	{
+	    // Code here.
+	    for(int i=0;i<32 and i<=n;i++)
+	    {
+	        if(__builtin_popcount(n-i)==i)
+	        return 0;
+	    }
+	    return 1;
+	}
+};
